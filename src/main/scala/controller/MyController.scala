62eccb8bf1ad4b034fdfed2cef53ff51f6983a65
petitviolet <violethero0820@gmail.com>
2017-10-07 21:48:08 +0900

    modified controller routings

--- src/main/scala/controller/MyController.scala
+++ src/main/scala/controller/MyController.scala
@@ -10,6 +10,11 @@ class MyController extends WebApp {
     s"HELLO!!!! cookie: ${cookies}, params: ${params}, request: ${request}, path: ${requestPath}"
   }
 
+  get("/echo/*") {
+    logger.info(s"`/echo`")
+    multiParams("splat").head
+  }
+
   get("/say-hello") {
     logger.info(s"`/say-hello` params: $params")
     s"Hello, ${params.getOrElse("name", "Anonymous")}!\n"
